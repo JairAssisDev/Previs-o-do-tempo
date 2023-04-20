949844bdfd15ab354c0505a670395e490e737ec4
Jair Victor Dev <63867699+JairAssisDev@users.noreply.github.com>
2023-04-20 13:46:36 -0300

    finaldofim

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function colocarDadosnatela(dados){
     document.querySelector(".cidade").innerHTML= "Tempo em "+dados.name
     document.querySelector(".temp").innerHTML= Math.floor(dados.main.temp)+ "°C"
     document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
-    document.querySelector(".umidade").innerHTML = dados.main.humidity +" % de sUmidade relativa do ar"
+    document.querySelector(".umidade").innerHTML = dados.main.humidity +" % de Umidade relativa do ar"
     document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`  
 }
 
