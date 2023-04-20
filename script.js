ef6e104a2d90c52987c343056e781f2320f13bfc
Jair Victor Dev <63867699+JairAssisDev@users.noreply.github.com>
2023-04-20 13:35:17 -0300

    atlz

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+const key="cb62809e73ace71e0a1111cf1637a71c"
+
+function colocarDadosnatela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML= "Tempo em "+dados.name
+    document.querySelector(".temp").innerHTML= Math.floor(dados.main.temp)+ "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity +" %"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/$04n.png`  
+}
+
+async function buscarCidade(cidade){
+    const dados= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(Resposta => Resposta.json())
+    colocarDadosnatela(dados)
+}
+function cliqueNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+    
+    buscarCidade(cidade)
+}
